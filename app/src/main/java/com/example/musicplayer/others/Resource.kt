25fe517ef1e97c643660e757296f085b19ff7e4b
mayank01000@gmail.com <Mayankawesome007@>
2022-03-24 02:36:17 +0530

    Music Service And Notification Implemented

---
+++ app/src/main/java/com/example/musicplayer/others/Resource.kt
@@ -0,0 +1,22 @@
+package com.example.musicplayer.others
+
+import android.content.res.Resources
+import java.security.KeyStore
+
+data class Resource<out T>(val status: Status, val data:T?, val message:String? ) {
+
+    companion object{
+        fun <T> success(data: T) = Resource(Status.SUCESS, data, null)
+
+        fun <T> error(message: String?, data: T?) = Resource(Status.ERROR, data, message)
+
+        fun <T> Loading(data: T) = Resource(Status.LOADING, data, null)
+    }
+
+}
+
+enum class Status{
+    SUCESS,
+    ERROR,
+    LOADING
+}
